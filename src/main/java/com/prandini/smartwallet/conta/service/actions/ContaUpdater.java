a377254932962dececd281998f1bea2cb4149b1f
KaioOliveira <kaio.oliveira@maps.com.br>
2024-07-10 13:09:07 -0300

    feat: updater conta

--- src/main/java/com/prandini/smartwallet/conta/service/actions/ContaUpdater.java
+++ src/main/java/com/prandini/smartwallet/conta/service/actions/ContaUpdater.java
@@ -9,17 +9,22 @@ import com.prandini.smartwallet.conta.domain.Conta;
 import com.prandini.smartwallet.conta.repository.ContaRepository;
 import com.prandini.smartwallet.lancamento.domain.Lancamento;
 import jakarta.annotation.Resource;
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Component
+@CommonsLog
 public class ContaUpdater {
 
     @Resource
     private ContaRepository repository;
 
     public void atualizaLancamentoSaida(Long id, Lancamento lancamento){
+        log.info("Atualizando lancamentos de saida.");
+        log.info("REVER LOGICA");
+
         Conta conta = repository.getReferenceById(id);
 
         conta.getLancamentos().add(lancamento);
@@ -28,6 +33,9 @@ public class ContaUpdater {
     }
 
     public void atualizaLancamentoEntrada(Long id, BigDecimal valor){
+        log.info("Atualizando lancamentos de entrada.");
+        log.info("REVER LOGICA");
+
         Conta conta = repository.getReferenceById(id);
 
         conta.setSaldoParcial(conta.getSaldoParcial().add(valor));
