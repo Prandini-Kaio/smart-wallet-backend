b944016b446ce7a77ef597ef7ec7ca774913097b
Prandini <prandini.kaio@gmail.com>
2024-12-22 18:06:05 -0300

    fix: remove cors required

---
+++ src/main/java/com/prandini/smartwallet/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.prandini.smartwallet.config;
+/*
+* @author prandini
+* created 12/22/24
+*/
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "http://popcorn.internal:4200", "http://192.168.1.16:4200", "http://192.168.1.21:4200");
+    }
+}
