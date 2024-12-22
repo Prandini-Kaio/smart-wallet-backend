e0c206afea9d4d733c60ef119fa915d6b769fb04
Prandini <prandini.kaio@gmail.com>
2024-12-22 11:36:50 -0300

    feat: add a default profile to production

--- src/main/java/com/prandini/smartwallet/SmartWalletBackendApplication.java
+++ src/main/java/com/prandini/smartwallet/SmartWalletBackendApplication.java
@@ -2,12 +2,14 @@ package com.prandini.smartwallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com.prandini.smartwallet"})
 @EnableJpaRepositories(basePackages = "com.prandini.smartwallet", repositoryImplementationPostfix = "CustomImpl")
 @EnableScheduling
+@Profile("db-local")
 public class SmartWalletBackendApplication {
 
     public static void main(String[] args) {
