af34fce8596dd269cc8e095e08d7aa3daacf6a79
Prandini <prandini.kaio@gmail.com>
2024-12-22 11:35:26 -0300

    fix: fix cors config

--- src/main/java/com/prandini/smartwallet/config/CorsConfig.java
+++ src/main/java/com/prandini/smartwallet/config/CorsConfig.java
@@ -17,7 +17,9 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
 
