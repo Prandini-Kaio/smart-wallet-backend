cba800a2046407ab61aabd6d8647f714d64e18d1
KaioOliveira <kaio.oliveira@maps.com.br>
2025-05-15 19:41:02 -0300

    refactor: Refatoracao da aplicacao. Entidade base para recorrencia

--- src/main/java/com/prandini/smartwallet/recorrencia/domain/FrequenciaRecorrencia.java
+++ src/main/java/com/prandini/smartwallet/recorrencia/domain/FrequenciaRecorrencia.java
@@ -4,5 +4,9 @@ package com.prandini.smartwallet.recorrencia.domain;
  * @author kaiooliveira
  * created 05/05/2025
  */
-public class FrequenciaRecorrencia {
+public enum FrequenciaRecorrencia {
+    DIARIA,
+    SEMANAL,
+    MENSAL,
+    ANUAL
 }
--- src/main/java/com/prandini/smartwallet/recorrencia/domain/Recorrencia.java
+++ src/main/java/com/prandini/smartwallet/recorrencia/domain/Recorrencia.java
@@ -1,8 +1,38 @@
 package com.prandini.smartwallet.recorrencia.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
 /**
  * @author kaiooliveira
  * created 05/05/2025
  */
+
+@Entity
+@Table(name = "RECORRENCIA")
 public class Recorrencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private FrequenciaRecorrencia frequencia;
+
+    @Column(name = "DATA_INICIO")
+    private LocalDate dataInicio;
+
+    @Column(name = "DATA_FIM")
+    private LocalDate dataFim;
+
+    @Column(name = "ATIVA")
+    private Boolean ativa = true;
 }
